6355f845a3c4052341665a9dcbf64ab2f3e30771
NicolasLerch <lerchnicolas@hotmail.com>
2024-06-20 17:55:48 -0300

    done language change feature

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -6,6 +6,10 @@ const projects = JSON.parse(projectsPath);
 const skillPath = fs.readFileSync(path.join(__dirname, '../data/skills.json'), 'utf-8')
 const skills = JSON.parse(skillPath)
 
+const skillsEs = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/skillsEs.json'), 'utf-8'))
+const projectsEs = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/projects-es.json'), 'utf-8'))
+
+
 const controller = {
     mainPage: function(req, res){
         res.render('index')
@@ -32,6 +36,34 @@ const controller = {
             
     },
     downloadCV: function(req, res){
+        res.download(path.join(__dirname, '../../public/Lerch_CV_en.pdf'))
+    },
+
+    // rutas en español
+    mainPageEs: function(req, res){
+        res.render('es/index-es')
+    },
+    contactEs: function(req, res){
+        res.render('es/contact-es')
+    },
+    skillsEs: function(req, res){
+        res.render('es/skills-es', {skills: skillsEs})
+    },
+    projectsEs: function(req, res){
+        res.render('es/projects-es', {projects: projectsEs})
+    },
+    projectDetailEs: function(req, res){
+        let idExists = projectsEs.find(project => project.id == req.params.id);
+        if(isNaN(req.params.id) || !idExists){
+            return res.redirect('/404')
+        }            
+        const project = projectsEs.find(project => project.id == req.params.id)
+        res.render('es/projectDetail-es', {project: project})
+    },
+    aboutEs: function(req, res){
+        res.render('es/about-es')
+    },
+    downloadCVEs: function(req, res){
         res.download(path.join(__dirname, '../../public/LerchNicolas_CV.pdf'))
     }
 }
